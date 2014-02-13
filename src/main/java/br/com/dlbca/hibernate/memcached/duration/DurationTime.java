e867486a2a457d50662c49871a6dabc666a3aa05
Renan Scarela <renan.scarela@gmail.com>
2014-02-13 14:20:37 -0200

    DurationTime constants has been added.

--- src/main/java/br/com/dlbca/hibernate/memcached/duration/DurationTime.java
+++ src/main/java/br/com/dlbca/hibernate/memcached/duration/DurationTime.java
@@ -9,10 +9,28 @@ package br.com.dlbca.hibernate.memcached.duration;
 
 public final class DurationTime {
 
+	public static final int ONE_MINUTE = 60;
+
+	public static final int FIVE_MINUTES = 60*5;
+	
+	public static final int TEN_MINUTES = 60*10;
+	
+	public static final int FIFTEEN_MINUTES = 60*15;
+	
+	public static final int THIRTY_MINUTES = 60*30;
+	
+	public static final int FOURTY_FIVE_MINUTES = 60*45;
+	
+	public static final int ONE_HOUR = 60*60;
+	
+	public static final int ONE_DAY = 60*60*24;
+	
 	/**
 	 * Maximum value(30 days) allowed for cache, following
 	 * memcached specification
 	 */
 	public static final int MAX = 60*60*24*30;
+	
+	
 	
 }
